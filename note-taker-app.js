0bd789379355307a29fd87526219d8dd8f067808
alex broder <alexbroder119@gmail.com>
2020-12-02 03:03:28 +0200

    quick update on the js and classes of bootstrap
    added comments and some class to align the notes better and and added the functionality that if there are more then 20 letter it will display only 20 with 3 dots at the end

--- note-taker-app.js
+++ note-taker-app.js
@@ -1,3 +1,4 @@
+//all the variables that needed at the start
 let submit = document.getElementById('submit');
 let noteField = document.getElementById('noteField');
 let outPut = document.getElementById('outPut');
@@ -5,13 +6,15 @@ let noteCounter = 0;
 let modalBody = document.getElementById('modal-body');
 
 let addNote = () => {
+
+  //creating all the variables and elements that needed and setting up the styles
   let h5 = document.createElement('h5');
   let div = document.createElement('div');
   div.setAttribute('id', 'noteDiv');
   div.setAttribute('class', 'card');
   div.style.width = 400 + 'px';
   div.style.height = 150 + 'px';
-  div.appendChild(h5);
+  div.style.margin = '25px';
   let divCardBody = document.createElement('div');
   divCardBody.setAttribute('class', 'card-body');
   let p = document.createElement('p');
@@ -21,27 +24,34 @@ let addNote = () => {
   btnModal.setAttribute('value', 'see more');
   btnModal.setAttribute('data-target', '#Modal');
   btnModal.setAttribute('data-toggle', 'modal');
-  btnModal.style.width = 100 + 'px';
+  btnModal.style.width = '100px';
   btnDelete = document.createElement('input');
   btnDelete.setAttribute('class', 'btn btn-outline-danger');
   btnDelete.setAttribute('value', 'delete');
   btnDelete.setAttribute('onclick', 'removeNote(this)');
-  btnDelete.style.width = 100 + 'px';
+  btnDelete.style.width = '100px';
+  btnDelete.style.margin = '5px';
+  h5.style.textAlign = 'center';
   h5.innerHTML = `Note`;
+  //appending all of the elements (children)
   outPut.appendChild(div);
+  div.appendChild(h5);
   div.appendChild(divCardBody);
   divCardBody.appendChild(p);
   divCardBody.appendChild(btnModal);
   divCardBody.appendChild(btnDelete);
   let letterCount = noteField.value.split(/\W/).join('').length;
+  //counts how many letters there are in the value
   if (letterCount <= 20) {
     p.innerHTML = noteField.value;
     btnModal.style.display = 'none';
   } else {
-    p.innerHTML = 'click to see the whole note';
+    //checks if there is more then 20 letters and if there is the only 20 of the letters will appear
+    p.innerHTML = `${noteField.value.slice(0, 20)}...`;
     modalBody.innerHTML = noteField.value;
   }
 }
+//removes the note
 let removeNote = (btn) => {
   let note = document.getElementById('noteDiv');
   note.remove(btn.parentNode.parentNode);
